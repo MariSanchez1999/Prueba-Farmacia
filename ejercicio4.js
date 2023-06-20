4908ff24945b8e0edaece9059d12283f6ad093e8
MariSanchez1999 <122774818+MariSanchez1999@users.noreply.github.com>
2023-06-20 15:21:56 -0600

    Ejercicio4
    Suma los elementos de la diagonal de una matriz cuadrada de 4x4.
    Salida esperada:
    La suma es: 62

---
+++ ejercicio4.js
@@ -0,0 +1,14 @@
+const matriz = [
+  [17, 9, 36, 21],
+  [8, 28, 3, 1],
+  [15, 7, 5, 30],
+  [10, 17, 4, 12]
+];
+
+let sumaDiagonal = 0;
+
+for (let i = 0; i < matriz.length; i++) {
+  sumaDiagonal += matriz[i][i];
+}
+
+console.log("La suma es:", sumaDiagonal);
